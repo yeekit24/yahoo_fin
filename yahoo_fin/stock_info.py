17d4b31f505ee7e92dfd159c4c51a0f0871dfc67
yeekit24 <yeekit24@gmail.com>
2021-03-07 00:52:55 +0800

    fix underscore bug

--- yahoo_fin/stock_info.py
+++ yahoo_fin/stock_info.py
@@ -56,11 +56,11 @@ def _convert_to_numeric(s):
 
     if "M" in s:
         s = s.strip("M")
-        return force_float(s) * 1_000_000
+        return force_float(s) * 1000000
 
     if "B" in s:
         s = s.strip("B")
-        return force_float(s) * 1_000_000_000
+        return force_float(s) * 1000000000
 
     return force_float(s)
 
